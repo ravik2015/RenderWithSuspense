affdb944c5e45276dd238000c4e1368201237285
Saurabh chauhan <saurabhchauhan@isteams-Mac-mini12.local>
2019-12-02 15:37:43 +0530

    code added for react native

--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
 import React, {Suspense} from 'react';
-import ReactDOM from 'react-dom';
-
-import './styles.css';
+import {View, Text} from 'react-native';
 import {fetchProfileData} from './fakeApi';
 
 const resource = fetchProfileData();
 
-function ProfilePage() {
+const ProfilePage = () => {
   return (
-    <Suspense fallback={<h1>Loading profile...</h1>}>
+    <Suspense fallback={<Text style={{fontSize: 24}}>Loading profile...</Text>}>
       <ProfileDetails />
-      <Suspense fallback={<h1>Loading posts...</h1>}>
+      <Suspense fallback={<Text style={{fontSize: 24}}>Loading posts...</Text>}>
         <ProfileTimeline />
       </Suspense>
     </Suspense>
   );
-}
+};
 
 function ProfileDetails() {
   // Try to read user info, although it might not have loaded yet
   const user = resource.user.read();
-  return <h1>{user.name}</h1>;
+  return (
+    <View>
+      <Text style={{fontSize: 24}}>{user.name}</Text>
+    </View>
+  );
 }
 
 function ProfileTimeline() {
   // Try to read posts, although they might not have loaded yet
   const posts = resource.posts.read();
   return (
-    <ul>
+    <View>
       {posts.map(post => (
-        <li key={post.id}>{post.text}</li>
+        <Text key={post.id} style={{fontSize: 24}}>
+          {post.text}
+        </Text>
       ))}
-    </ul>
+    </View>
   );
 }
-
-const rootElement = document.getElementById('root');
-ReactDOM.createRoot(rootElement).render(<ProfilePage />);
+export default ProfilePage;
